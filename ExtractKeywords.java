bd94930fc0c1ac01c71aad303ccd984927e275bf
Redamancy <Diancong.Wu@gmail.com>
2021-09-23 13:40:42 +0800

    1.2-Debug concerning Tab and Space

--- ExtractKeywords.java
+++ ExtractKeywords.java
@@ -20,68 +20,71 @@ public class ExtractKeywords {
 		int level[] = {1,2,3,4};
 		String reader;
 		FileInputStream inputStream = new FileInputStream(f_path);
-	    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-	    int num_else = 0;
-	    int num_if_else_if_else = 0;
-	    int num_if_else = 0;
-	    int num_keyWords = 0;
-	    int count = 0;
-        
-	    String []key=  {
-        		"auto","double","int","struct","break","else","long","switch","case","enum","register",
-        		"typedef","char","extern","return","union", "const","float","short","unsigned","continue",
-        		"for","signed","void","default","goto","sizeof","volatile","do","if","while","static",
-        		"elseif"};
-        StringBuilder str =new StringBuilder();
-        while((reader = bufferedReader.readLine()) != null) {
-        	reader = reader.replace(':', ' ');
-            if(reader.matches("(.*)//(.*)")){
-            	String b[] = reader.split("//");
-                str.append(b[0] + " ");
-            }else{
-            	str.append(reader + " ");
-            }
-        }
-        inputStream.close();
-        bufferedReader.close();
-        
-        //Refactor the code read in
-        String s = str.toString();
-        Pattern pat = Pattern.compile("\"(.*?)\"");
-        Matcher mat = pat.matcher(s);
-        while(mat.find()){
-            s = s.replace(mat.group()," ");
-            pat = Pattern.compile("\"(.*?)\"");
-            mat = pat.matcher(s);
-        }
-        pat = Pattern.compile("/\\**(.*?)/");
-        mat = pat.matcher(s);
-        while(mat.find()){
-            s = s.replace(mat.group()," ");
-            mat = pat.matcher(s);
-        }
-        if(s.isEmpty()){
-        	System.out.println("Wrong Format");
-            System.exit(0);
-        }
-        
-        s=s.replace("["," ");
-        s=s.replace("]"," ");
-        s=s.replace("-","a");
-        s=s.replace("*","a");
-        s=s.replace("/","a");
-        s=s.replace("+","a");
-        s=s.replace(">","a");
-        s=s.replace("=","a");
-        s=s.replace("!","a");
-        s=s.replace(":","a");
-        s=s.replace("\\","a");
-        s= s.replaceAll("[^a-zA-Z]", " ");
-        String []s1=s.split("[  ' ']");
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+		int num_else = 0;
+		int num_if_else_if_else = 0;
+		int num_if_else = 0;
+		int num_keyWords = 0;
+		int count = 0;
+		
+		String []key=  {
+				"auto","double","int","struct","break","else","long","switch","case","enum","register",
+				"typedef","char","extern","return","union", "const","float","short","unsigned","continue",
+				"for","signed","void","default","goto","sizeof","volatile","do","if","while","static",
+				"elseif"};
+		StringBuilder str =new StringBuilder();
+		while((reader = bufferedReader.readLine()) != null) {
+			reader = reader.replace(':', ' ');
+		    if(reader.matches("(.*)//(.*)")){
+		    	String b[] = reader.split("//");
+		        str.append(b[0] + " ");
+		    }else{
+		    	str.append(reader + " ");
+		    }
+		}
+		inputStream.close();
+		bufferedReader.close();
+		
+		//Refactor the code read in
+		String s = str.toString();
+		Pattern pat = Pattern.compile("\"(.*?)\"");
+		Matcher mat = pat.matcher(s);
+		while(mat.find()){
+		    s = s.replace(mat.group()," ");
+		    pat = Pattern.compile("\"(.*?)\"");
+		    mat = pat.matcher(s);
+		}
+		pat = Pattern.compile("/\\**(.*?)/");
+		mat = pat.matcher(s);
+		while(mat.find()){
+		    s = s.replace(mat.group()," ");
+		    mat = pat.matcher(s);
+		}
+		if(s.isEmpty()){
+			System.out.println("Wrong Format");
+		    System.exit(0);
+		}
+		
+		s=s.replace("["," ");
+		s=s.replace("]"," ");
+		s=s.replace("-","a");
+		s=s.replace("*","a");
+		s=s.replace("/","a");
+		s=s.replace("+","a");
+		s=s.replace(">","a");
+		s=s.replace("=","a");
+		s=s.replace("!","a");
+		s=s.replace(":","a");
+		s=s.replace("\\","a");
+		s= s.replaceAll("[^a-zA-Z]", " ");
+		String []s1=s.split("[  ' ']");
 	    
 	    
 	    
 	    
+
+
+
 		
     }
 }
